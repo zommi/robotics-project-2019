945db42ba4d20128bc64fb501a989cf4e08f0cd3
tommaso <tommaso.pegolotti@hotmail.com>
2019-05-22 11:18:53 +0200

    lolo

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zommi/Desktop/robotics-project-2019/src
-# Build directory: /home/zommi/Desktop/robotics-project-2019/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/zommi/Desktop/robotics-project-2019/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/zommi/Desktop/Ros-Workspace/devel;/opt/ros/melodic".split(';') if "/home/zommi/Desktop/Ros-Workspace/devel;/opt/ros/melodic" != "" else []
