fb42642cac88dc57dc1b072c97d15bf28f17939b
tommaso <tommaso.pegolotti@hotmail.com>
2019-07-06 20:04:52 +0200

    add tf

--- src/project2/src/odom.cpp
+++ src/project2/src/odom.cpp
@@ -1,18 +1,40 @@
 #include "ros/ros.h"
 #include <geometry_msgs/PointStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+
 #include <math.h>
 
 #define BASELINE 1.30
 #define FRONT_REAR_WHEELS_DISTANCE 1.765
 #define STEERING_FACTOR 18
 
-double x_k = 0;
+class odom_pub_sub
+{
 
-double y_k = 0;
+private:
+  ros::NodeHandle n;
+  ros::Subscriber sub;
+  ros::Publisher pub;
+  tf::TransformBroadcaster odom_broadcaster;
+  geometry_msgs::TransformStamped odom_trans;
+  double x_k;
+  double y_k;
+  double theta_k;
+  double t_k;
+  nav_msgs::Odometry odom_msg;
 
-double theta_k = 0;
+public:
 
-double t_k = 0;
+odom_pub_sub()
+{
+  x_k = 0;
+  y_k = 0;
+  theta_k = 0;
+  t_k = 0;
+  sub = n.subscribe("/speedsteer",1000,&odom_pub_sub::odom_callback,this);
+  pub = n.advertise<nav_msgs::Odometry>("/odom",1);
+}
 
 void odom_callback(const geometry_msgs::PointStamped::ConstPtr& msg)
 {
@@ -31,16 +53,36 @@ void odom_callback(const geometry_msgs::PointStamped::ConstPtr& msg)
   t_k = msg->header.stamp.nsec;
 
   std::cout << "x = " << x_k << "\ny = " << y_k << "\ntheta = " << theta_k << "\n";
+
+  geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(theta_k);
+
+  odom_trans.header.stamp = msg->header.stamp;
+  odom_trans.header.frame_id = "odom";
+  odom_trans.child_frame_id = "base_link";
+
+  odom_trans.transform.translation.x = x_k;
+  odom_trans.transform.translation.y = y_k;
+  odom_trans.transform.translation.z = 0.0;
+  odom_trans.transform.rotation = odom_quat;
+  odom_broadcaster.sendTransform(odom_trans);
+
+
+  odom_msg.pose.pose.position.x = x_k;
+  odom_msg.pose.pose.position.y = y_k;
+  odom_msg.pose.pose.position.z = 0.0;
+  odom_msg.pose.pose.orientation = odom_quat;
+  odom_msg.header = msg->header;
+  odom_msg.header.frame_id = "odom";
+  odom_msg.child_frame_id = "base_link";
+
+  pub.publish(odom_msg);
 }
+};
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "odom");
-
-  ros::NodeHandle n;
-
-  ros::Subscriber sub = n.subscribe("/speedsteer",1000,odom_callback);
-
+  ros::init(argc, argv, "main2");
+  odom_pub_sub pub_sub;
   ros::spin();
 
   return 0;
